4a7296d94a9f8dc0986f918a5e1a47bf21407f3e
Sangram Mohite <mohite.sangram@gmail.com>
2019-10-12 12:57:04 +0530

    Datatable status colored

--- src/components/Datatable.vue
+++ src/components/Datatable.vue
@@ -2,26 +2,31 @@
     <v-card>
         <v-card-title>
             <v-text-field
-                    v-model="search"
                     append-icon="mdi-magnify"
+                    hide-details
                     label="Search"
                     single-line
-                    hide-details
+                    v-model="search"
             ></v-text-field>
         </v-card-title>
-    <v-data-table
-            :headers="headers"
-            :items="data"
-            :items-per-page="5"
-            class="elevation-1"
-    ></v-data-table>
-        </v-card>
+        <v-data-table
+                :headers="headers"
+                :items="data"
+                :items-per-page="5"
+                class="elevation-1"
+        >
+
+            <template v-slot:item.status="{ item }">
+                <v-chip :color="`${getColor(item.status)}`">{{ item.status }}</v-chip>
+            </template>
+        </v-data-table>
+    </v-card>
 </template>
 
 <script>
-        export default {
-            name: "Datatable",
-        data () {
+    export default {
+        name: "Datatable",
+        data() {
             return {
                 search: '',
                 headers: [
@@ -31,9 +36,9 @@
                         sortable: false,
                         value: 'name',
                     },
-                    { text: 'Gas Type', value: 'type' },
-                    { text: 'Owner', value: 'owner' },
-                    { text: 'Status', value: 'status' }
+                    {text: 'Gas Type', value: 'type'},
+                    {text: 'Owner', value: 'owner'},
+                    {text: 'Status', value: 'status'}
 
                 ],
                 data: [
@@ -100,20 +105,34 @@
                 ],
             }
         },
+        methods: {
+            getColor(status) {
+                console.log(status);
+                switch (status) {
+                    case "Booked":
+                        return "orange";
+                    case "Available":
+                        return "green";
+                    case "Empty":
+                        return "#999999";
+                    case "Lost":
+                        return "brown";
+                }
+            }
+        }
     }
 </script><
 
 <style>
-    .data.Booked
-     {
-         background: orange !important;
-     }
-    .data.Available
-    {
+    .data.Booked {
+        background: orange !important;
+    }
+
+    .data.Available {
         background: green !important;
     }
-    .data.Empty
-    {
+
+    .data.Empty {
         background: black !important;
     }
 </style>
--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -1,34 +1,35 @@
 <template>
     <v-card
-            flat
             class="mx-auto"
-            max-width = "900">
-       <!--        row one is for "add new cylinder"-->
-    <v-container style="padding:0;" >
-        <v-row>
-            <v-col>
-            <v-btn
-                    absolute
-                    right
-                    rounded
-                    :color="this.$vuetify.theme.themes.light.inventory"
-                    class="white--text">
-                <span> Add Cylinder</span>
-            </v-btn>
-            </v-col>
-        </v-row>
-        <v-row class="ma-10">
-            <v-col>
-                <Datatable/>
-            </v-col>
-         </v-row>
+            flat
+            max-width="900">
+        <!--        row one is for "add new cylinder"-->
+        <v-container style="padding:0;">
+            <v-row>
+                <v-col>
+                    <v-btn
+                            :color="this.$vuetify.theme.themes.light.inventory"
 
-   </v-container>
+                            class="white--text float-right"
+
+                            rounded>
+                        <span> Add Cylinder</span>
+                    </v-btn>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <Datatable/>
+                </v-col>
+            </v-row>
+
+        </v-container>
     </v-card>
 </template>
 
 <script>
     import Datatable from '../components/Datatable'
+
     export default {
         components: {Datatable},
     };
